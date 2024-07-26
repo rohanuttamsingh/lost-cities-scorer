9adefa9d8fd2ca7d3bf50539682f31e4b2f08970
Rohan Uttamsingh <rohanuttamsingh@gmail.com>
2024-07-26 00:10:13 -0400

    static site layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,55 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const colors = ["Yellow", "White", "Green", "Blue", "Red"];
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <h1>Lost Cities Scorer</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Round</th>
+            <th>Player 1</th>
+            <th>Player 2</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>0</td>
+            <td>0</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>0</td>
+            <td>0</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>0</td>
+            <td>0</td>
+          </tr>
+          <tr>
+            <td>Total</td>
+            <td>0</td>
+            <td>0</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div className="round-scoring">
+        {colors.map((color) => (
+          <div key={color} className="round-scoring-column">
+            <div className="round-scoring-column-header">{color}</div>
+            <div>H</div>
+            {[...Array(9).keys()].map((num) => (
+              <div key={num}>{num + 2}</div>
+            ))}
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
