0eaf4691d5e77eb75677937d4ce17b85a1a3074c
Rohan Uttamsingh <rohanuttamsingh@gmail.com>
2024-08-18 20:21:47 +0530

    player selector

--- src/Round.tsx
+++ src/Round.tsx
@@ -2,25 +2,55 @@ import { useState } from "react";
 import City from "./City";
 import "./Round.css";
 
+type Player = 0 | 1;
+
 type RoundProps = {
   colors: Array<string>;
 };
 
 export default function Round({ colors }: RoundProps) {
-  const [score, setScore] = useState(0);
+  const [player, setPlayer] = useState<Player>(0);
+  const [scores, setScores] = useState([0, 0]);
 
-  const updateScore = (scoreDiff: number) => {
-    setScore(score + scoreDiff);
+  const updateScores = (player: 0 | 1, scoreDiff: number) => {
+    setScores(
+      scores.map((score, i) => (i === player ? score + scoreDiff : score))
+    );
   };
 
   return (
     <>
-      <div className="round-table">
+      <div className="player-heading">Player {player + 1}</div>
+      <div
+        className={`round-table ${
+          player === 0 ? "player-turn" : "not-player-turn"
+        }`}
+      >
         {colors.map((color) => (
-          <City key={color} color={color} updateScore={updateScore} />
+          <City
+            key={color}
+            color={color}
+            updateScore={(scoreDiff: number) => updateScores(0, scoreDiff)}
+          />
         ))}
       </div>
-      <div>Round: {score}</div>
+      <div
+        className={`round-table ${
+          player === 1 ? "player-turn" : "not-player-turn"
+        }`}
+      >
+        {colors.map((color) => (
+          <City
+            key={color}
+            color={color}
+            updateScore={(scoreDiff: number) => updateScores(1, scoreDiff)}
+          />
+        ))}
+      </div>
+      <div className="player-select">
+        <button onClick={() => setPlayer(0)}>Player 1: {scores[0]}</button>
+        <button onClick={() => setPlayer(1)}>Player 2: {scores[1]}</button>
+      </div>
     </>
   );
 }
